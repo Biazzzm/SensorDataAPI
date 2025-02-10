13c4c91958f63d5dceaaf454f56327ec1d39a98d
Igor do Nascimento Rodrigues <101749315+nascimentoIgorSinerlog@users.noreply.github.com>
2025-02-10 19:57:53 -0300

    fix: add swagger

--- SensorDataAPI/Program.cs
+++ SensorDataAPI/Program.cs
@@ -35,12 +35,8 @@ builder.Services.AddHostedService<TelegramPollingService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 //app.UseHttpsRedirection();
 
